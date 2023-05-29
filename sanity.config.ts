ffe09f5dc6b3bf97b6aefa865279bfc506ce9bd7
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-05-29 09:23:43 +0000

    Sanity setup

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "sanity";
-
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
 
 const config = defineConfig({
-    projectId: 'wxsjgdk5',
-    dataset: 'production',
-    title: 'My Portfolio Website',
-    
-
-})
+  projectId: 'wxsjgdk5',
+  dataset: 'production',
+  title: 'My Portfolio Website',
+  apiVersion: '2023-05-29',
+  basePath: '/admin',
+  plugins: [deskTool()],
+});
 
 export default config;
