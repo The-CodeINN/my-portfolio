a76e1cfe99e9c1a33a3090c0e566a7750e26d1b0
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-05-29 01:29:30 +0000

    added sanity cms

---
+++ sanity.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "sanity";
+
+
+const config = defineConfig({
+    projectId: 'wxsjgdk5',
+    dataset: 'production',
+    title: 'My Portfolio Website',
+    
+
+})
+
+export default config;
