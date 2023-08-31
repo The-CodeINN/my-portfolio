d1f7ea37d397a6570f22cf81ecb93eb9650497bc
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-08-31 14:04:48 +0000

    Working on the homepage

---
+++ app/data/social.ts
@@ -0,0 +1,34 @@
+import { Linkedin, Twitter, Github, Facebook, Instagram } from 'lucide-react';
+
+export const socialLinks = [
+  {
+    id: 1,
+    name: 'GitHub',
+    url: 'https://github.com/The-CodeINN',
+    icon: Github,
+  },
+  {
+    id: 2,
+    name: 'Twitter',
+    url: 'https://www.twitter.com/thecodeinn',
+    icon: Twitter,
+  },
+  {
+    id: 3,
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/richard-emijere-84067bb0/',
+    icon: Linkedin,
+  },
+  {
+    id: 4,
+    name: 'Facebook',
+    url: 'https://www.facebook.com/emijere.richard/',
+    icon: Facebook,
+  },
+  {
+    id: 5,
+    name: 'Instagram',
+    url: 'https://www.instagram.com/richie_richy112/',
+    icon: Instagram,
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 import { getProfile } from '@/lib/sanity.query';
 import type { ProfileType } from '@/types';
 import HeroSvg from './icons/HeroSvg';
+import { Slide } from './animation/Slide';
 
 export default async function Home() {
   const profile: ProfileType[] = await getProfile();
 
   return (
-    <main className='max-w-7xl mx-auto md:px-16 lg:mt-32 mt-20'>
+    <main className='max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20'>
       <section className='flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 mb-16'>
         {profile &&
           profile.map((data) => (
             <div key={data._id} className='lg:max-w-2xl max-w-2xl'>
-              <h1>{data.headline}</h1>
+              <Slide>
+                <h1 className='font-black tracking-tight text-3xl sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full'>
+                  {data.headline}
+                </h1>
+                <p className='text-base dark:text-zinc-400 text-zinc-600 leading-relaxed'>
+                  {data.shortBio}
+                </p>
+              </Slide>
+              {/* <Slide delay={0.1}>
+                <Social
+              </Slide> */}
             </div>
           ))}
+          <Slide delay={0.1}>
         <HeroSvg />
+          </Slide>
       </section>
     </main>
   );
